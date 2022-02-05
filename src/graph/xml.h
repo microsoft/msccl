1e38d4650ec68591e455068a42b2ce9b688f9821
Saeed Maleki <saemal@microsoft.com>
2022-02-05 02:46:03 +0000

    increasing xml node limits

--- src/graph/xml.h
+++ src/graph/xml.h
@@ -12,7 +12,7 @@
 #define MAX_STR_LEN 255
 #define MAX_ATTR_COUNT 16
 #define MAX_SUBS 1024
-#define MAX_NODES (1<<12) 
+#define MAX_NODES (1<<22) 
 
 #define NODE_TYPE_NONE 0
 #define NODE_TYPE_OPEN 1
