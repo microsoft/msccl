667952a6df8854acd43138f2f380cc511c03710d
Saeed Maleki <saemal@microsoft.com>
2022-02-10 22:17:05 +0000

    adding the checks for alltoall with SCCL_CONFIG

--- src/collectives/all_to_all.cc
+++ src/collectives/all_to_all.cc
@@ -13,19 +13,43 @@ ncclResult_t ncclAllToAll(const void* sendbuff, void* recvbuff, size_t sendcount
     ncclDataType_t datatype, ncclComm_t comm, cudaStream_t stream) {
   size_t allcount = sendcount*comm->nRanks;
   size_t nbytes = allcount*ncclTypeSize(datatype);
-  for (int scclAlgoIndex = 0; scclAlgoIndex < comm->numberOfSCCAlgorithms; scclAlgoIndex++) {
-    struct scclAlgorithm* scclAlgo = &comm->scclAlgos[scclAlgoIndex];
-    if ((scclAlgo->isValid) && (scclAlgo->collectiveType == ncclFuncAllToAll) && (comm->nRanks == scclAlgo->ngpus) 
-        && ((allcount % comm->scclAlgos[scclAlgoIndex].nchunksPerLoop) == 0)
-        && (nbytes >= scclAlgo->minBytes) && (nbytes < scclAlgo->maxBytes)){
-      NVTX3_FUNC_RANGE_IN(nccl_domain);
-      struct ncclInfo info = { ncclFuncAllToAll, "AllToAll",
-        sendbuff, recvbuff, 0 /* all-to-all can only be out of place */, sendcount, datatype, ncclSum, 0, comm, stream, /* Args */
-        SCCL_CHUNKSTEPS, SCCL_SLICESTEPS };
-      info.scclAlgoIndex = scclAlgoIndex;
-      return ncclEnqueueCheck(&info);
+
+  if (comm->nScclRegistrations > 0) {
+    for (int i = 0; i < comm->nScclRegistrations; ++i) {
+      struct scclRegistration *reg = &comm->scclRegistrations[i];
+      if (reg->minBytes <= nbytes && (nbytes < reg->maxBytes || reg->maxBytes == -1)) {
+        struct scclAlgorithm* scclAlgo = &comm->scclAlgos[reg->algoIndex];
+        if ((scclAlgo->isValid) && (scclAlgo->collectiveType == ncclFuncAllToAll) && (comm->nRanks == scclAlgo->ngpus) 
+            && ((allcount % scclAlgo->nchunksPerLoop) == 0)){
+          NVTX3_FUNC_RANGE_IN(nccl_domain);
+          struct ncclInfo info = { ncclFuncAllToAll, "AllToAll",
+            sendbuff, recvbuff, 0 /* all-to-all can only be out of place */, sendcount, datatype, ncclSum, 0, comm, stream, /* Args */
+            SCCL_CHUNKSTEPS, SCCL_SLICESTEPS };
+          info.scclAlgoIndex = reg->algoIndex;
+	  auto curProto = scclAlgo->protocol;
+	  scclAlgo->protocol = reg->protocol;
+          auto ret = ncclEnqueueCheck(&info);
+	  scclAlgo->protocol = curProto;
+	  return ret;
+        }
+      }
+    }
+  } else {
+    for (int scclAlgoIndex = 0; scclAlgoIndex < comm->numberOfSCCAlgorithms; scclAlgoIndex++) {
+      struct scclAlgorithm* scclAlgo = &comm->scclAlgos[scclAlgoIndex];
+      if ((scclAlgo->isValid) && (scclAlgo->collectiveType == ncclFuncAllToAll) && (comm->nRanks == scclAlgo->ngpus) 
+          && ((allcount % comm->scclAlgos[scclAlgoIndex].nchunksPerLoop) == 0)
+          && (nbytes >= scclAlgo->minBytes) && (nbytes < scclAlgo->maxBytes)){
+        NVTX3_FUNC_RANGE_IN(nccl_domain);
+        struct ncclInfo info = { ncclFuncAllToAll, "AllToAll",
+          sendbuff, recvbuff, 0 /* all-to-all can only be out of place */, sendcount, datatype, ncclSum, 0, comm, stream, /* Args */
+          SCCL_CHUNKSTEPS, SCCL_SLICESTEPS };
+        info.scclAlgoIndex = scclAlgoIndex;
+        return ncclEnqueueCheck(&info);
+      }
     }
   }
+
   // If there is no proper SCCL algorithm, then use p2p
   NCCLCHECK(ncclGroupStart());
   for (int r=0; r<comm->nRanks; r++){
