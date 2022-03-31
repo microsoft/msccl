bdc0e9767b4ba625437cab22a87621119e553db2
Saeed Maleki <saemal@microsoft.com>
2022-03-31 00:43:35 +0000

    removing manual LL

--- src/collectives/device/all_reduce.h
+++ src/collectives/device/all_reduce.h
@@ -624,7 +624,7 @@ template<class FUNC, typename T, int UNROLL>
 class ncclFunction<ncclFuncAllReduce, NCCL_ALGO_SCCL, NCCL_PROTO_LL, FUNC, T, UNROLL> {
   public:
     __device__ void run(struct ncclWorkElem* args) {
-      scclFunctionManual<FUNC, T, UNROLL> scclfunc;
+      scclFunctionLL<FUNC, T, UNROLL> scclfunc;
       scclfunc.run(args, 1);
     }
 };
