77bf9ce0465def03e27a331cd2fd7a167e6799a7
Saeed Maleki <saemal@microsoft.com>
2022-05-06 18:27:59 +0000

    fixing a bug in custom collective API

--- src/collectives/custom_collective.cc
+++ src/collectives/custom_collective.cc
@@ -7,9 +7,9 @@
 #include "enqueue.h"
 
 NCCL_API(ncclResult_t, ncclCustomCollective, const void* sendbuff, void* recvbuff, size_t count,
-    ncclDataType_t datatype, int scclAlgorithmIndex, ncclComm* comm, cudaStream_t stream);
+    ncclDataType_t datatype, int scclAlgorithmIndex, ncclComm_t comm, cudaStream_t stream);
 ncclResult_t ncclCustomCollective(const void* sendbuff, void* recvbuff, size_t count,
-    ncclDataType_t datatype, int scclAlgorithmIndex, ncclComm* comm, cudaStream_t stream) {
+    ncclDataType_t datatype, int scclAlgorithmIndex, ncclComm_t comm, cudaStream_t stream) {
   NVTX3_FUNC_RANGE_IN(nccl_domain);
   
   struct ncclInfo info = { ncclFuncCustomCollective, "CustomCollective",
