1d3f0a4917dd415eec57cf2d95475c2441d4cd8a
Saeed Maleki <saemal@microsoft.com>
2022-05-25 07:22:42 +0000

    temp fix for strided copy

--- src/collectives/device/stride_copy.cu
+++ src/collectives/device/stride_copy.cu
@@ -1,6 +1,6 @@
 #include "devcomm.h"
 
-static int strideMemcpyGridsize = 0, strideMemcpyBlocksize = 0;
+/*static int strideMemcpyGridsize = 0, strideMemcpyBlocksize = 0;
 
 // memory stride copy kernel
 template <typename T>
@@ -12,14 +12,14 @@ __global__ void strideMemcpyKernel(T *__restrict__ out, const T *__restrict__ in
     out[j] = in[i];
   }
 }
-
+*/
 cudaError_t strideMemcpyAsync(void *dst, const void *src, const size_t size, const int height, const int width, cudaStream_t stream) {
-  if (strideMemcpyGridsize == 0 || strideMemcpyBlocksize == 0)
+/*  if (strideMemcpyGridsize == 0 || strideMemcpyBlocksize == 0)
     cudaOccupancyMaxPotentialBlockSize(&strideMemcpyGridsize, &strideMemcpyBlocksize, strideMemcpyKernel<uint4>);
 
   if (size < sizeof(uint4))
     strideMemcpyKernel<char><<<strideMemcpyGridsize, strideMemcpyBlocksize, 0, stream>>>((char*)dst, (char*)src, size, height, width);
   else
-    strideMemcpyKernel<uint4><<<strideMemcpyGridsize, strideMemcpyBlocksize, 0, stream>>>((uint4*)dst, (uint4*)src, size/sizeof(uint4), height, width);
+    strideMemcpyKernel<uint4><<<strideMemcpyGridsize, strideMemcpyBlocksize, 0, stream>>>((uint4*)dst, (uint4*)src, size/sizeof(uint4), height, width);*/
   return cudaSuccess;
 }
