021f0eea86092b8a99305b054208d8648c3bebeb
Saeed Maleki <saemal@microsoft.com>
2022-09-08 20:49:22 +0000

    removing unnecessary printfs

--- src/enqueue.cc
+++ src/enqueue.cc
@@ -1173,8 +1173,6 @@ ncclResult_t ncclEnqueueCollKernel(struct ncclComm* comm, struct ncclQueueElem* 
   struct ncclWorkElem* elem = work->elems;
   struct ncclProxyOp* proxyOp = &eqElem->proxyOp;
   int mscclAlgoIndex = elem->mscclWork.mscclAlgoIndex;
-  INFO(NCCL_COLL, "QQQQ elem nChannels %d funIndex %d mscclAlgoIndex %d count %d type %d", 
-    elem->nChannels, elem->header.funcIndex, mscclAlgoIndex, elem->count, elem->header.type);
   if (mscclAlgoIndex >= 0){
     // short circuit and only save proxy
     struct mscclAlgorithm* mscclAlgo = &comm->mscclHostComm.mscclDevComm.mscclAlgos[mscclAlgoIndex];
@@ -1247,7 +1245,6 @@ void CUDART_CB ncclEnqueueHostSetup(void* arg) {
     } else {
       NCCLCHECKGOTO(ncclEnqueueCollKernel(comm, eqElem, aggMode), ret, cb_end);
     }
-    INFO(NCCL_COLL, "eqInfo->elemList->count() %d. CUDA graph mode %d", eqInfo->elemList->count(), USING_CUDA_GRAPH);
     eqElem = eqInfo->elemList->getNext();
   }
 
@@ -1256,7 +1253,7 @@ void CUDART_CB ncclEnqueueHostSetup(void* arg) {
 
 cb_end:
   if (ret != ncclSuccess) {
-    WARN("Failure in host setup : %s. CUDA graph mode %d", ncclGetErrorString(ret), USING_CUDA_GRAPH);
+    WARN("Failure in host setup : %s.", ncclGetErrorString(ret));
   }
   eqInfo->ret = ret;
 }
